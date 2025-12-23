74d9aa913b3780596c26ff428b118dff7d11f5c8
Darsh Matariya <darshmatariya@gmail.com>
2025-12-23 16:34:58 +0530

    connecting db with atlas

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONOGDB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error : ", error);
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
- 
+// require('dotenv').config({path: './.env'})
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/*
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONOGDB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on ${process.env.PORT}`);
+            
+        })
+
+    } catch (error) {
+        console.log("ERROR: ", error);
+        throw error;
+    }
+} )()
+*/
