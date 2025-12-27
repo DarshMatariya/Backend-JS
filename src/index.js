0d918593f787790ae79adcd17d0b310a9b83f3db
Darsh Matariya <darshmatariya@gmail.com>
2025-12-27 10:37:06 +0530

    models and jwt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require('dotenv').config({path: './.env'})
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
+
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT || 8000}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
